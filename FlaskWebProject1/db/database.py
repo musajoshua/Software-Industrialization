82c7c8b682f00395aa4fe812dc7fb00121079bf3
musajoshua <gidijosh@gmail.com>
2026-06-10 10:50:11 +0200

    chore: change db port enviroment variable to 3306

--- FlaskWebProject1/db/database.py
+++ FlaskWebProject1/db/database.py
@@ -3,7 +3,7 @@ import mysql.connector
 
 mydb = mysql.connector.connect(
     host=os.environ.get("DB_HOST", "host.docker.internal"),
-    port=int(os.environ.get("DB_PORT", "33061")),
+    port=int(os.environ.get("DB_PORT", "3306")),
     user=os.environ.get("DB_USER", "root"),
     password=os.environ["DB_PASSWORD"],
 )
